168d6f858488eac92366933c0b97d44b7080a5f5
menahem_erlich <erlichmenahem@gmail.com>
2025-10-23 09:58:35 +0300

    create_card

--- utils/deck.py
+++ utils/deck.py
@@ -1,8 +1,26 @@
 def create_card(rank:str,suite:str) -> dict:
-    pass
+    card = {}
+    list_nume = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    card["rank"] = rank
+    card["suite"] = suite
+    if rank == "A":
+        card["value"] = 14
+    elif rank == "K":
+        card["value"] = 13
+    elif rank == "Q":
+        card["value"] = 12
+    elif rank == "J":
+        card["value"] = 11
+    elif rank in list_nume:
+        card["value"] = int(rank)
+    else:
+        print("Typo error!")
+    return card
 
 def compare_cards(p1_card:dict, p2_card:dict) -> str:
     pass
+
+
 
 def create_deck() -> list[dict]:
     pass
