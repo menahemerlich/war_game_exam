d1ffac196f4389f848bf38118e2d792f4d2ded92
menahem_erlich <erlichmenahem@gmail.com>
2025-10-23 11:04:26 +0300

    init_game

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,3 +1,6 @@
+from war_game_esam.utils.deck import shuffle, create_deck
+
+
 def create_player(*name:str) -> dict:
     player = {}
     if len(name) == 0:
@@ -9,7 +12,16 @@ def create_player(*name:str) -> dict:
     return player
 
 def init_game()->dict:
-    pass
+    game_dict = {}
+    p1 = create_player("erlich")
+    p2 = create_player()
+    new_deck = shuffle(create_deck())
+    p1["hand"] = new_deck[:26]
+    p2["hand"] = new_deck[26:]
+    game_dict["deck"] = new_deck
+    game_dict["player_1"] = p1
+    game_dict["player_2"] = p2
+    return game_dict
 
 def play_round(p1:dict,p2:dict):
     pass
