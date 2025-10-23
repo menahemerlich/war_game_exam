8f2f5fbd22670c3f3993e234d5fa489dcaf2aeec
menahem_erlich <erlichmenahem@gmail.com>
2025-10-23 10:41:58 +0300

    shuffle

--- utils/deck.py
+++ utils/deck.py
@@ -1,4 +1,4 @@
-
+import random
 
 def create_card(rank:str,suite:str) -> dict:
     card = {}
@@ -44,3 +44,11 @@ def create_deck() -> list[dict]:
     return deck
 
 def shuffle(deck:list[dict]) -> list[dict]:
+    count = 0
+    while count < 1000:
+        index1 = random.randint(0,51)
+        index2 = random.randint(0, 51)
+        if index1 != index2:
+            deck[index1], deck[index2] = deck[index2], deck[index1]
+            count += 1
+    return deck
